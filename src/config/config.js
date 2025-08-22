6268890ea88296a6ffd261e73ac2cc954293c54c
Hiro <hiro.sys16@gmail.com>
2025-08-22 23:26:03 +0600

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   serverUrl: import.meta.env.VITE_APP_SERVERURL,
-  localUrl: import.meta.env.VITE_APP_LOCALURL || "http://localhost:3030/api/v1",
+  localUrl: import.meta.env.VITE_APP_LOCALURL || "http://168.220.234.100:3030/api/v1",
   proxyUrl: import.meta.env.VITE_APP_PROXYURL,
 };
 
